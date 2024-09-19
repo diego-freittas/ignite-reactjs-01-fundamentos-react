faa0971f261b9a45a9236bc05f6a7e90513821f4
Diego de Souza Freitas <tpddiego@gmail.com>
2024-09-19 16:58:57 -0300

    cria e estiliza o componente Comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,7 @@
+import { Comment } from './Comment.jsx';
 import styles from './Post.module.css';
+
+
 export function Post() {
   return (
     <article className={styles.post}>
@@ -22,6 +25,22 @@ export function Post() {
           <a href="">#rocketseat</a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+
     </article>
   )
 }
