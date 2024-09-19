5e62937b8ed11a4ac78f21c5177f5c5efcb74a2f
Diego de Souza Freitas <tpddiego@gmail.com>
2024-09-19 17:10:28 -0300

    Ajusta o component Avatar, Post, Comment e Sidebar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,16 +1,19 @@
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
+
 
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/diego-freittas.png" alt="" />
+      <Avatar hasBorder={false} src="https://github.com/diego-freittas.png" alt="" />
+
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
-              <strong>Diego Fernandes</strong>
+              <strong>Diego Freitas</strong>
               <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
             </div>
             <button title="Deletar comentário">
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
+
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
